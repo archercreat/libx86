b8a0d791b7c323f6ed3de3f81214a07e553de7ed
archercreat <archercreat@gmail.com>
2022-02-02 21:24:27 +0300

    added example usage

---
+++ example/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <libx86/disasm.hpp>
+
+int test()
+{
+    int x = 0;
+    return x ^ 5 * 10 ^ 1337;
+}
+
+int main() 
+{
+    std::vector<uint8_t> raw(100);
+    raw.assign((const uint8_t*)test, (const uint8_t*)test + 100);
+    triton::API api;
+    api.setArchitecture(triton::arch::ARCH_X86_64);
+    api.setConcreteMemoryAreaValue(0, raw);
+    auto r = x86::unroll(&api, 0);
+    r.dump();
+    return EXIT_SUCCESS;
+}
